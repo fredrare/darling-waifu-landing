058faba405eec491c4bd3498cc1f134fbbb6cb44
Franco Ramirez <francoramirezreyes@gmail.com>
2021-11-26 02:03:30 -0500

    404 in useEffect

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { navigate } from "gatsby";
 
 const Page404 = () => {
-  navigate("/");
+  useEffect(() => {
+    navigate("/");
+  }, []);
   return <></>;
 };
 
