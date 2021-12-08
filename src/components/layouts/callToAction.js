4f988d2523c02663346eb0f5e1181c1a1f06ef8d
Franco Ramirez <francoramirezreyes@gmail.com>
2021-12-08 11:25:10 -0500

    paper corrected

--- src/components/layouts/callToAction.js
+++ src/components/layouts/callToAction.js
@@ -21,11 +21,11 @@ const CTA = () => {
       <div className="w-full flex flex-col space-y-4 content-center items-center justify-center">
         <div className="-px-4 flex flex-row space-x-4 content-center items-center justify-center">
           <Button
-            link={paper}
+            link={paperdoc}
             title="Read our litepaper"
           >
             <div className="flex flex-row">
-              <img src={paperdoc} alt="Litepaper" className="mr-4" />
+              <img src={paper} alt="Litepaper" className="mr-4" />
               <div>Litepaper</div>
             </div>
           </Button>
